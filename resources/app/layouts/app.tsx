f7502638914aed98f1c2152e3924e518af53d48b
pphatdev <leatsophat02@gmail.com>
2024-11-25 00:15:04 +0700

    🌈: Update App layout

--- resources/app/layouts/app.tsx
+++ resources/app/layouts/app.tsx
@@ -3,6 +3,7 @@ import { SidebarProvider } from "@/components/ui/sidebar";
 import { AdminSidebar } from "@/layouts/admin-sidebar";
 import { BrowserRouter } from "react-router-dom";
 import { AdminHeader } from './admin-header';
+import { cn } from "@/lib/utils";
 
 export default function Layout({ children, className }: { children: React.ReactNode, className?: string }) {
     return (
@@ -11,7 +12,7 @@ export default function Layout({ children, className }: { children: React.ReactN
                 <AdminSidebar />
                 <main className="w-full">
                     <AdminHeader/>
-                    <div className="w-full max-w-3xl mx-auto h-full p-5">
+                    <div className={cn("w-full mx-auto h-full p-5", className)}>
                         { children }
                     </div>
                 </main>
