c57b9bc858b0927af548595bbd1a60f9a2087923
pphatdev <leatsophat02@gmail.com>
2024-11-24 17:51:22 +0700

    🌈: Created new admin sidebar

---
+++ resources/app/layouts/admin-header.tsx
@@ -0,0 +1,29 @@
+import { SearchCommand } from '@/components/search-command';
+import { SidebarTrigger } from '@/components/ui/sidebar';
+import { Notifications } from '@/components/notifications';
+import React from 'react';
+import { AdminHeaderProfile } from '@/components/admin-header-profile';
+
+
+export const AdminHeader = () => {
+    return (
+        <header className='bg-background w-full border-b'>
+            <div className="z-50 flex items-center justify-between px-2 mx-auto max-w-8xl">
+                <div className="flex items-center justify-between w-64 gap-4 px-1 py-2">
+                    <SidebarTrigger />
+                </div>
+                <nav className="flex items-center justify-end gap-4 px-4 py-2">
+                    <ul className="flex items-center justify-end gap-2">
+                        <li className="flex items-center">
+                            <SearchCommand />
+                            <Notifications />
+                        </li>
+                        <li>
+                            <AdminHeaderProfile />
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    )
+}
