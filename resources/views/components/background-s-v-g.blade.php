ed7b7b308eb6b5370c74f38c588e7b993d54ebe3
pphatdev <leatsophat02@gmail.com>
2024-11-24 11:38:37 +0700

    🌈: Created background SVG

---
+++ resources/views/components/background-s-v-g.blade.php
@@ -0,0 +1,68 @@
+<svg width="3628" height="3074" viewBox="0 0 3628 3074" fill="none" class="fixed w-[200%] -left-3 h-[200%] inset-0 opacity-10 -z-10 lg:-left-[45%] -top-1/2" xmlns="http://www.w3.org/2000/svg">
+    <g filter="url(#filter0_f_1_25)">
+        <path d="M498.872 2066.42C774.696 1461.84 932.038 1193.91 1205.17 518.288C1478.29 -157.338 1246.04 527.998 1246.04 527.998C1246.04 527.998 1060.22 1000.72 929.848 1276.2C769.994 1613.96 498.872 2066.42 498.872 2066.42Z" fill="url(#paint0_linear_1_25)" />
+    </g>
+    <g filter="url(#filter1_f_1_25)">
+        <path d="M128.001 2812.9C519.679 2276.07 967.602 1457.24 1370.91 850.275C1774.22 243.309 1409 868 1409 868C1409 868 643.065 2569.24 460 2812.9C235.539 3111.66 128.001 2812.9 128.001 2812.9Z" fill="url(#paint1_radial_1_25)" />
+    </g>
+    <g filter="url(#filter2_f_1_25)">
+        <path d="M326 2304.18C585.877 1692.57 736.14 1420.62 991.457 738.061C1246.78 55.5067 1032.57 746.697 1032.57 746.697C1032.57 746.697 859.212 1224.13 736.108 1502.93C585.165 1844.77 326 2304.18 326 2304.18Z" fill="url(#paint2_radial_1_25)" />
+    </g>
+    <g style="mix-blend-mode:color-dodge" filter="url(#filter3_f_1_25)">
+        <path d="M326 2278.76C587.979 1668.05 739.176 1396.61 996.84 714.941C1254.5 33.2687 1037.92 723.718 1037.92 723.718C1037.92 723.718 862.924 1200.55 738.861 1478.93C586.744 1820.25 326 2278.76 326 2278.76Z" fill="url(#paint3_linear_1_25)" />
+    </g>
+    <g filter="url(#filter4_f_1_25)">
+        <path d="M391 2361.22C695.609 1790.66 1102 1039.5 1361.5 409C1621 -221.498 1461 409 1461 409C1461 409 1101.11 1340.3 951.5 1601.22C768.056 1921.14 391 2361.22 391 2361.22Z" fill="url(#paint4_linear_1_25)" />
+    </g>
+    <defs>
+        <filter id="filter0_f_1_25" x="370.872" y="92.5068" width="1095.27" height="2101.91" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+            <feFlood flood-opacity="0" result="BackgroundImageFix" />
+            <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+            <feGaussianBlur stdDeviation="64" result="effect1_foregroundBlur_1_25" />
+        </filter>
+        <filter id="filter1_f_1_25" x="0.000488281" y="457.032" width="1688.6" height="2616.65" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+            <feFlood flood-opacity="0" result="BackgroundImageFix" />
+            <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+            <feGaussianBlur stdDeviation="64" result="effect1_foregroundBlur_1_25" />
+        </filter>
+        <filter id="filter2_f_1_25" x="198" y="308.926" width="1046.67" height="2123.25" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+            <feFlood flood-opacity="0" result="BackgroundImageFix" />
+            <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+            <feGaussianBlur stdDeviation="64" result="effect1_foregroundBlur_1_25" />
+        </filter>
+        <filter id="filter3_f_1_25" x="198" y="286.234" width="1053.07" height="2120.53" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+            <feFlood flood-opacity="0" result="BackgroundImageFix" />
+            <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+            <feGaussianBlur stdDeviation="64" result="effect1_foregroundBlur_1_25" />
+        </filter>
+        <filter id="filter4_f_1_25" x="263" y="0.778809" width="1374.79" height="2488.44" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+            <feFlood flood-opacity="0" result="BackgroundImageFix" />
+            <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+            <feGaussianBlur stdDeviation="64" result="effect1_foregroundBlur_1_25" />
+        </filter>
+        <linearGradient id="paint0_linear_1_25" x1="1280.19" y1="206.994" x2="-74.1814" y2="1242.64" gradientUnits="userSpaceOnUse">
+            <stop stop-color="#00E0FF" />
+            <stop stop-color="#DBFF00" />
+            <stop offset="1" stop-color="#FF53B0" />
+        </linearGradient>
+        <radialGradient id="paint1_radial_1_25" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(729.678 1995.3) rotate(122.393) scale(1002.98 396.606)">
+            <stop stop-color="#FF0000" />
+            <stop stop-color="#FA00FF" />
+            <stop offset="1" stop-color="#53F5FF" />
+        </radialGradient>
+        <radialGradient id="paint2_radial_1_25" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(726.81 1371.53) rotate(109.299) scale(1002.98 396.606)">
+            <stop stop-color="#FF0000" />
+            <stop stop-color="#FA00FF" />
+            <stop offset="1" stop-color="#53F5FF" />
+        </radialGradient>
+        <linearGradient id="paint3_linear_1_25" x1="1064.75" y1="402.019" x2="-265.672" y2="1468.25" gradientUnits="userSpaceOnUse">
+            <stop stop-color="#00E0FF" />
+            <stop stop-color="#FF58B2" />
+            <stop offset="0.835" stop-color="#14BC9E" />
+        </linearGradient>
+        <linearGradient id="paint4_linear_1_25" x1="982.5" y1="899.72" x2="558.5" y2="2360.72" gradientUnits="userSpaceOnUse">
+            <stop stop-color="#00E0FF" />
+            <stop offset="1" stop-color="#8A67C0" />
+        </linearGradient>
+    </defs>
+</svg>
