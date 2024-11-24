2c2cf96fdd9a914722a417f29304dad186a2db0d
pphatdev <leatsophat02@gmail.com>
2024-11-24 17:25:31 +0700

    🌈: Update Sidebar

---
+++ resources/app/components/admin-sidebar-footer.tsx
@@ -0,0 +1,87 @@
+"use client"
+
+import * as React from "react"
+import { Check, ChevronsUpDown, Cog } from "lucide-react"
+
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import {
+    Command,
+    CommandEmpty,
+    CommandGroup,
+    CommandInput,
+    CommandItem,
+    CommandList,
+} from "@/components/ui/command"
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from "@/components/ui/popover"
+
+const frameworks = [
+    {
+        value: "Account Settings",
+        label: "Account Settings",
+    },
+    {
+        value: "Themes",
+        label: "Themes",
+    },
+    {
+        value: "Logout",
+        label: "Logout",
+    },
+]
+
+export function AdminSidebarFooter() {
+    const [open, setOpen] = React.useState(false)
+    const [value, setValue] = React.useState("")
+
+    return (
+        <Popover open={open} onOpenChange={setOpen}>
+            <PopoverTrigger asChild>
+                <Button
+                    variant="outline"
+                    role="combobox"
+                    aria-expanded={open}
+                    className="w-full justify-between"
+                >
+                    <Cog/>
+                    {value
+                        ? frameworks.find((framework) => framework.value === value)?.label
+                        : "Settings"}
+                    <ChevronsUpDown className="opacity-50" />
+                </Button>
+            </PopoverTrigger>
+            <PopoverContent className="w-full p-0">
+                <Command>
+                    <CommandInput placeholder="Search settings..." className="h-9" />
+                    <CommandList>
+                        <CommandEmpty>No settings.</CommandEmpty>
+                        <CommandGroup>
+                            {frameworks.map((framework) => (
+                                <CommandItem
+                                    key={framework.value}
+                                    value={framework.value}
+                                    onSelect={(currentValue) => {
+                                        setValue(currentValue === value ? "" : currentValue)
+                                        setOpen(false)
+                                    }}
+                                >
+                                    {framework.label}
+                                    <Check
+                                        className={cn(
+                                            "ml-auto",
+                                            value === framework.value ? "opacity-100" : "opacity-0"
+                                        )}
+                                    />
+                                </CommandItem>
+                            ))}
+                        </CommandGroup>
+                    </CommandList>
+                </Command>
+            </PopoverContent>
+        </Popover>
+    )
+}
