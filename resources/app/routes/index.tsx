732ae158f1c112a2d94a55702b83aebb936a10b0
PPhatDev <leatsophat02@gmail.com>
2024-11-26 18:10:03 +0700

    🪲: Updated Routes

--- resources/app/routes/index.tsx
+++ resources/app/routes/index.tsx
@@ -1,6 +1,7 @@
-import { AuthMiddleware } from '@/middlewares/auth';
+// import { AuthMiddleware } from '@/middlewares/auth';
 import NotFound from '@/pages/404';
-import Dashboard from '@/pages/dashboards/page';
+import Dashboard from '@/pages/admin/dashboard/page';
+import Blogs from '@/pages/blogs/page';
 import HelloWorld from '@/pages/hello-world';
 import React from 'react';
 
@@ -14,12 +15,21 @@ const routes = [
         element: <NotFound/>,
     },
     {
-        path: "/app",
+        path: "app",
         children: [
             {
                 path: "",
                 element: (
                     <Dashboard></Dashboard>
+                    // <AuthMiddleware>
+                    //     <HelloWorld/>
+                    // </AuthMiddleware>
+                ),
+            },
+            {
+                path: "blog",
+                element: (
+                    <Blogs></Blogs>
                     // <AuthMiddleware>
                     //     <HelloWorld/>
                     // </AuthMiddleware>
